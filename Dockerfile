63c8f93fbf73ea883b4df751396125b18e61ca91
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-04-16 18:50:19 -0300

    ch3

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# pull base image
+FROM python:3.7
+
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# set work dir
+WORKDIR /code
+
+# install requirements
+COPY Pipfile Pipfile.lock /code/
+RUN pip install pipenv && pipenv install --system
+
+# copy project
+COPY . /code/
---
+++ users/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username',)
