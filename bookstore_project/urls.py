3353972066a87fec10a35e7521d75dcf01614f52
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-04-17 14:41:50 -0300

    added accounts endpoint for django auth and custom user auth

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
