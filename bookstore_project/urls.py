b4565683dcbd6c2e166d3abcb8df9c681c9b6682
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-04-18 16:53:52 -0300

    updated a couple of production level settings for my multimillion dollar app

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
+    path('books/', include('books.urls')),
 ]
