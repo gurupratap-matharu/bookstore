a81c7506a7f1f824a556ceab7c39cc9d224110c5
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-04-18 15:05:42 -0300

    added env variables to docker-compose and reading them from settings

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nq#osk$vggb3z756w%h8%s*as%=5pduig9h0bo7eb6yub+3=)x'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
