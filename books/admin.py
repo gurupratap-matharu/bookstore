5a99908897db363e14beb99691d80acf302f663d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-04-19 12:56:01 -0300

    added review model to tabular inline admin

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline, ]
     list_display = ('title', 'author', 'price',)
 
 
