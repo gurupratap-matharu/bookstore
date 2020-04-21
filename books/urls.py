5e826d1a795a59222c5d3a455ce4b83a83b6bea7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-04-21 16:29:25 -0300

    added search functionality to homepage to filter relevant books

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BookDetailView, BookListView
+from .views import BookDetailView, BookListView, SearchResultsListView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<uuid:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
-from django.shortcuts import render
+from django.db.models import Q
 from django.views.generic import DetailView, ListView
 
 from .models import Book
@@ -19,3 +19,15 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
