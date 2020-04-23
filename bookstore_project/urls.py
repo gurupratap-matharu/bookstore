551e9b3b4c59abcb406372408d75e3385aa081ae
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-04-23 13:59:53 -0300

    changed the admin url for more protection

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('anything-but-admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
