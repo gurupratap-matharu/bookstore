72a5c2e6597f1e0c838dc5f877a2da232c7a624e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-04-23 15:24:25 -0300

    secure_proxy_ssl_header and allowed_hosts update

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['bookstore-sandbox.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -190,6 +190,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
