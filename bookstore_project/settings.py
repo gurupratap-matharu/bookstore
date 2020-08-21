355bb3d66b47d47bff0d3bc92365c02a5724444b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-21 12:27:58 -0300

    added heroku domain to allowed hosts

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['bookstore-sandbox.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['whispering-mountain-43664.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
