d54eb6b58c58b3580acaa8158d1d2357e677a48d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-22 19:07:49 -0300

    added success messages

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import FormView, TemplateView
 
 from pages.forms import ContactForm, FeedbackForm
@@ -15,20 +16,22 @@ class AboutPageView(TemplateView):
     template_name = 'about.html'
 
 
-class ContactPageView(FormView):
+class ContactPageView(SuccessMessageMixin, FormView):
     form_class = ContactForm
     template_name = 'pages/contact.html'
     success_url = '/'
+    success_message = 'Message sent successfully!'
 
     def form_valid(self, form):
         form.send_mail()
         return super().form_valid(form)
 
 
-class FeedbackPageView(FormView):
+class FeedbackPageView(SuccessMessageMixin, FormView):
     form_class = FeedbackForm
     template_name = 'pages/feedback.html'
     success_url = '/'
+    success_message = 'Feedback sent successfully!'
 
     def form_valid(self, form):
         form.send_mail()
