315c9fe632a29f916dcf0bf2c5dd08790677dc2d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-08-22 13:19:29 -0300

    added awesome icons

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import AboutPageView, HomePageView
+from pages.views import (AboutPageView, ContactPageView, FeedbackPageView,
+                         HomePageView)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('feedback/', FeedbackPageView.as_view(), name='feedback'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,10 @@
-from django.views.generic import TemplateView
+import logging
+
+from django.views.generic import FormView, TemplateView
+
+from pages.forms import ContactForm, FeedbackForm
+
+logger = logging.getLogger(__name__)
 
 
 class HomePageView(TemplateView):
@@ -7,3 +13,23 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class ContactPageView(FormView):
+    form_class = ContactForm
+    template_name = 'pages/contact.html'
+
+    def form_valid(self, form):
+        logger.info('received valid contact form = %s', form)
+        form.send_mail()
+        return super().form_valid(form)
+
+
+class FeedbackPageView(FormView):
+    form_class = FeedbackForm
+    template_name = 'pages/feedback.html'
+
+    def form_invalid(self, form):
+        logger.info('Received valid feedback form = %s', form)
+        form.send_mail()
+        return super().form_invalid(form)
