950ea3f41c61de42c785e24861268dd4fefb0236
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-04-11 22:38:53 -0300

    Add app name to pages url conf

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from pages.views import (AboutPageView, ContactPageView, FeedbackPageView,
                          HomePageView)
 
+app_name = 'pages'
+
+
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
