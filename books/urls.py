08aa3afe854c976e2ad35ca18e3e534fd5b5d01c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-04-11 23:07:13 -0300

    Add app name to urls

--- books/urls.py
+++ books/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from .views import BookDetailView, BookListView, SearchResultsListView
 
+app_name = "books"
+
+
 urlpatterns = [
-    path('', BookListView.as_view(), name='book_list'),
-    path('<uuid:pk>/', BookDetailView.as_view(), name='book_detail'),
-    path('search/', SearchResultsListView.as_view(), name='search_results'),
+    path("", BookListView.as_view(), name="book_list"),
+    path("<uuid:pk>/", BookDetailView.as_view(), name="book_detail"),
+    path("search/", SearchResultsListView.as_view(), name="search_results"),
 ]
