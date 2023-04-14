3ab5732b254336963853ec3f94d0967de427fa6f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-04-14 17:20:51 -0300

    Show link to download book

--- books/admin.py
+++ books/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Book, Review
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "price")
+    list_display = ("title", "author", "price", "download")
+
+    def download(self, obj):
+        url = obj.link
+        html = f'<a href="{url}" target="_blank">Link</a>' if url else ""
+        return mark_safe(html)  # nosec
 
 
 @admin.register(Review)
