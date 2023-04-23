20979a548061c17600dcd10ce5360df8fa335375
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-04-23 13:13:15 -0300

    Close  old db connections on cleanup

--- books/management/commands/startjobs.py
+++ books/management/commands/startjobs.py
@@ -9,6 +9,7 @@ from django.core.management.base import BaseCommand
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
+from django_apscheduler import util
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
@@ -17,6 +18,7 @@ from books.scrapers.sedici import SediciScraper
 logger = logging.getLogger(__name__)
 
 
+@util.close_old_connections
 def delete_old_job_executions(max_age=604_800):
     """
     Deletes all apscheduler job execution logs older than `max_age`.
