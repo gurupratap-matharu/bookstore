f56d967aff5b68e3bf6c4f0c9ff895f0b9c1c67a
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-04-23 11:59:36 -0300

    Improve the run time of the scraper

--- books/scrapers/sedici.py
+++ books/scrapers/sedici.py
@@ -29,6 +29,7 @@ class SediciScraper(BaseScraper):
 
     item_urls = set()
     items_data = list()
+
     item_urls_filename = str(CURRENT_DIR / "sedici_item_urls.txt")
     item_data_filename = str(CURRENT_DIR / "sedici_items.json")
 
@@ -38,13 +39,19 @@ class SediciScraper(BaseScraper):
 
         url = self.url
 
-        while url:
-            # Get list of all item urls (books)
-            bs = self.get_soup(url)
-            self.get_items(bs)
+        # while url:
+        #     # Get list of all item urls (books)
+        #     bs = self.get_soup(url)
+        #     self.get_items(bs)
 
-            # Update url to next page
-            url = self.get_next_page_link(bs)
+        #     # Update url to next page
+        #     url = self.get_next_page_link(bs)
+
+        # TODO: Remove these two lines and use the loop above
+        # For now lets take the 40 books and try to download them
+        
+        bs = self.get_soup(url)
+        self.get_items(bs)
 
         # Save all item urls to a file
         self.save_item_urls()
