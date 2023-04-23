040453c95e29c66ca009fe6fcc75cddd8232c220
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-04-23 11:59:56 -0300

    Add ap-scheduler to mgmt command

--- books/management/commands/startjobs.py
+++ books/management/commands/startjobs.py
@@ -2,10 +2,28 @@
 Django management command that triggers the scrapers.
 """
 
+import logging
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
 
 from books.models import Book
 from books.scrapers.sedici import SediciScraper
+
+logger = logging.getLogger(__name__)
+
+
+def delete_old_job_executions(max_age=604_800):
+    """
+    Deletes all apscheduler job execution logs older than `max_age`.
+    """
+
+    DjangoJobExecution.objects.delete_old_job_executions(max_age)
 
 
 class Command(BaseCommand):
@@ -22,7 +40,37 @@ class Command(BaseCommand):
         self.stdout.write(self.style.HTTP_BAD_REQUEST(msg))
 
     def handle(self, *args, **options):
-        sedici = SediciScraper()
-        sedici.run()
-        books = Book.objects.count()
-        self.stdout.write(f"Books: {books}")
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            SediciScraper().run,
+            trigger="interval",
+            minutes=2,
+            id="The Sedici Scraper",
+            max_instances=1,
+            replace_existing=True,
+        )
+
+        logger.info("Added job: The Sedici Scraper.")
+
+        scheduler.add_job(
+            delete_old_job_executions,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ),  # Midnight on Monday, before start of the next work week.
+            id="Delete Old Job Executions",
+            max_instances=1,
+            replace_existing=True,
+        )
+
+        logger.info("Added weekly job: Delete Old Job Executions.")
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
