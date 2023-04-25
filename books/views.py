66e1324ac664550b9480a5603a14329e83530f15
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2023-04-25 20:41:22 -0300

    Remove login required from books views

--- books/views.py
+++ books/views.py
@@ -1,23 +1,19 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.db.models import Q
 from django.views.generic import DetailView, ListView
 
 from .models import Book
 
 
-class BookListView(LoginRequiredMixin, ListView):
+class BookListView(ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "books/book_list.html"
-    login_url = "account_login"
 
 
-class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+class BookDetailView(DetailView):
     model = Book
     context_object_name = "book"
     template_name = "books/book_detail.html"
-    login_url = "account_login"
-    permission_required = "books.special_status"
 
 
 class SearchResultsListView(ListView):
